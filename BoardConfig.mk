f1828f766e17c0b9b15549effbbac40b86f6039d
yourKenga <yourkenga@gmail.com>
2017-11-28 15:53:32 +0300

    prada: add configs
    Change-Id: Icda74c8c94c129c450c6a9696e8c699819627fea

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,46 +15,112 @@
 #
 
 DEVICE_PATH := device/xiaomi/prada
-CM_PATH := vendor/cm/config/board
 
 # Define platform before including any common things
 include $(DEVICE_PATH)/PlatformConfig.mk
 
-# Inherit common ARM64 board fragments
-include $(CM_PATH)/common/arm64/architecture.mk
-include $(CM_PATH)/common/arm64/binder.mk
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
 
-# Inherit common board fragments
-include $(CM_PATH)/common/bluetooth.mk
-include $(CM_PATH)/common/bootloader.mk
-include $(CM_PATH)/common/camera.mk
-include $(CM_PATH)/common/clang.mk
-include $(CM_PATH)/common/cpusets.mk
-include $(CM_PATH)/common/dexopt.mk
-include $(CM_PATH)/common/dlmalloc.mk
-include $(CM_PATH)/common/filesystem.mk
-include $(CM_PATH)/common/gps.mk
-include $(CM_PATH)/common/sepolicy.mk
+# Second architecture
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
 
-# Inherit QCOM board fragments
-include $(CM_PATH)/qcom/bluetooth.mk
-include $(CM_PATH)/qcom/bootloader.mk
-include $(CM_PATH)/qcom/camera.mk
-include $(CM_PATH)/qcom/cne.mk
-include $(CM_PATH)/qcom/display.mk
-include $(CM_PATH)/qcom/encryption.mk
-include $(CM_PATH)/qcom/fm.mk
-include $(CM_PATH)/qcom/gps.mk
-include $(CM_PATH)/qcom/per-mgr.mk
-include $(CM_PATH)/qcom/platform.mk
-include $(CM_PATH)/qcom/power.mk
-include $(CM_PATH)/qcom/recovery.mk
-include $(CM_PATH)/qcom/ril.mk
-include $(CM_PATH)/qcom/sepolicy.mk
-include $(CM_PATH)/qcom/time.mk
+# Binder
+TARGET_USES_64_BIT_BINDER := true
 
-# Inherit Cyanogen board fragments
-include $(CM_PATH)/cyanogen/hardware.mk
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+
+# Camera
+USE_DEVICE_SPECIFIC_CAMERA := true
+
+# Clang
+USE_CLANG_PLATFORM_BUILD := true
+
+# CPUsets
+ENABLE_CPUSETS := true
+
+# Dexopt
+ifeq ($(HOST_OS),linux)
+    WITH_DEXPREOPT := true
+    WITH_DEXPREOPT_BOOT_IMG_ONLY := true
+endif
+
+# Malloc
+MALLOC_IMPL := dlmalloc
+
+# Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# GPS
+USE_DEVICE_SPECIFIC_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
+
+# SELinux
+BOARD_SEPOLICY_DIRS += \
+    $(DEVICE_PATH)/sepolicy
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH_QCOM := true
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := $(TARGET_BOARD_PLATFORM)
+
+# Camera
+BOARD_QTI_CAMERA_32BIT_ONLY := true
+
+# CNE / DPM
+BOARD_USES_QCNE := true
+TARGET_LDPRELOAD := libNimsWrap.so
+
+# Display
+TARGET_USES_C2D_COMPOSITION := true
+TARGET_USES_ION := true
+USE_OPENGL_RENDERER := true
+
+# Encryption
+TARGET_HW_DISK_ENCRYPTION := true
+
+# FM
+AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
+TARGET_QCOM_NO_FM_FIRMWARE := true
+
+# Peripheral manager
+TARGET_PER_MGR_ENABLED := true
+
+# QCOM support
+BOARD_USES_QCOM_HARDWARE := true
+
+# Power
+TARGET_POWERHAL_VARIANT := qcom
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
+
+# RIL
+TARGET_RIL_VARIANT := caf
+
+# SELinux
+include device/qcom/sepolicy/sepolicy.mk
+
+# Time
+BOARD_USES_QC_TIME_SERVICES := true
+
+# CM Hardware
+BOARD_USES_CYANOGEN_HARDWARE := true
+BOARD_HARDWARE_CLASS += \
+    hardware/cyanogen/cmhw
 
 # Inherit device-specific board fragments
 include $(DEVICE_PATH)/board/*.mk
--- device.mk
+++ device.mk
@@ -15,46 +15,177 @@
 #
 
 DEVICE_PATH := $(LOCAL_PATH)
-CM_PATH := vendor/cm/config/product
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
-# Include common product fragments
-include $(CM_PATH)/common/ant.mk
-include $(CM_PATH)/common/audio.mk
-include $(CM_PATH)/common/bluetooth.mk
-include $(CM_PATH)/common/bluetooth-le.mk
-include $(CM_PATH)/common/consumerir.mk
-include $(CM_PATH)/common/fingerprint.mk
-include $(CM_PATH)/common/fm.mk
-include $(CM_PATH)/common/gello.mk
-include $(CM_PATH)/common/gps.mk
-include $(CM_PATH)/common/lights.mk
-include $(CM_PATH)/common/media.mk
-include $(CM_PATH)/common/misc.mk
-include $(CM_PATH)/common/snap.mk
-include $(CM_PATH)/common/wifi.mk
+# ANT
+PRODUCT_PACKAGES += \
+    AntHalService \
+    com.dsi.ant.antradio_library \
+    libantradio
 
-# Include QCOM product fragments
-include $(CM_PATH)/qcom/audio.mk
-include $(CM_PATH)/qcom/camera.mk
-include $(CM_PATH)/qcom/cne.mk
-include $(CM_PATH)/qcom/display.mk
-include $(CM_PATH)/qcom/fm.mk
-include $(CM_PATH)/qcom/gps.mk
-include $(CM_PATH)/qcom/init.mk
-include $(CM_PATH)/qcom/media.mk
-include $(CM_PATH)/qcom/net.mk
-include $(CM_PATH)/qcom/power.mk
-include $(CM_PATH)/qcom/radio.mk
-include $(CM_PATH)/qcom/sensors.mk
-include $(CM_PATH)/qcom/usb.mk
-include $(CM_PATH)/qcom/wifi.mk
+PRODUCT_COPY_FILES += \
+    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
 
-# Include Cyanogen product fragments
-include $(CM_PATH)/cyanogen/livedisplay.mk
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio.r_submix.default \
+    audio.usb.default \
+    tinymix
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
+
+# Bluetooth
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml\
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
+
+# Consumerir
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    fingerprintd
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
+
+# FM
+PRODUCT_PACKAGES += \
+    libfmjni
+
+# Gello
+PRODUCT_PACKAGES += \
+    Gello
+
+# GPS
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
+
+# Lights
+PRODUCT_PACKAGES += \
+    lights.$(TARGET_BOARD_PLATFORM)
+
+# Media
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
+
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+
+# Misc dependencies
+PRODUCT_PACKAGES += \
+    libxml2
+
+# Snap
+PRODUCT_PACKAGES += \
+    Snap
+
+# Wi-Fi
+PRODUCT_PACKAGES += \
+    dhcpcd.conf \
+    hostapd \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.primary.$(TARGET_BOARD_PLATFORM) \
+    audiod \
+    libqcompostprocbundle \
+    libqcomvisualizer \
+    libqcomvoiceprocessing
+
+# Camera
+PRODUCT_PACKAGES += \
+    camera.$(TARGET_BOARD_PLATFORM)
+
+# CNE
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.cne.feature=1
+
+# Display
+PRODUCT_PACKAGES += \
+    copybit.$(TARGET_BOARD_PLATFORM) \
+    gralloc.$(TARGET_BOARD_PLATFORM) \
+    hwcomposer.$(TARGET_BOARD_PLATFORM) \
+    libtinyxml \
+    memtrack.$(TARGET_BOARD_PLATFORM)
+
+# FM
+PRODUCT_PACKAGES += \
+    FM2 \
+    libqcomfm_jni \
+    qcom.fmradio
+
+# GPS
+PRODUCT_PACKAGES += \
+    gps.conf \
+    gps.default \
+    libcurl
+
+# Init scripts
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    init.qcom.rc \
+    init.recovery.qcom.rc \
+    ueventd.qcom.rc
+
+# Media
+PRODUCT_PACKAGES += \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxCore \
+    libOmxEvrcEnc \
+    libOmxQcelp13Enc \
+    libOmxVdec \
+    libOmxVenc \
+    libstagefrighthw
+
+# IPv6
+PRODUCT_PACKAGES += \
+    ebtables \
+    ethertypes
+
+# QMI
+PRODUCT_PACKAGES += \
+    libjson
+
+# Power HAL
+PRODUCT_PACKAGES += \
+    power.$(TARGET_BOARD_PLATFORM)
+
+# Radio
+PRODUCT_PACKAGES += \
+    libcnefeatureconfig \
+    librmnetctl
+
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.$(TARGET_BOARD_PLATFORM)
+
+# Init scripts
+PRODUCT_PACKAGES += \
+    init.qcom.usb.rc
+
+# Wi-Fi
+PRODUCT_PACKAGES += \
+    libQWiFiSoftApCfg
+
+# Display calibration
+PRODUCT_PACKAGES += \
+    libjni_livedisplay
 
 # Include device-specific product fragments
 include $(LOCAL_PATH)/product/*.mk
